338e5a88af248608095977642eed61e38860ab12
Luciopicon <100102765+Luciopicon@users.noreply.github.com>
2026-05-19 15:22:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,35 +8,59 @@ const SSH_HOST = "185.194.204.52";
 const SSH_PORT = 22;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("WebSocket Proxy Online");
+  res.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+
+  res.end("WebSocket SSH Proxy Online");
 });
 
-const wss = new WebSocketServer({ server });
+const wss = new WebSocketServer({
+  server,
+  perMessageDeflate: false
+});
 
-wss.on("connection", (ws) => {
-  const socket = net.connect(SSH_PORT, SSH_HOST);
+wss.on("connection", (ws, req) => {
+  console.log("[WS] Cliente conectado");
+
+  ws.on("error", (err) => {
+    console.log("[WS ERROR]", err.message);
+  });
+
+  const socket = net.connect(SSH_PORT, SSH_HOST, () => {
+    console.log("[SSH] Conectado ao servidor SSH");
+  });
 
   socket.on("data", (data) => {
-    if (ws.readyState === ws.OPEN) {
-      ws.send(data);
+    try {
+      if (ws.readyState === ws.OPEN) {
+        ws.send(data);
+      }
+    } catch (e) {
+      console.log("[SEND ERROR]", e.message);
     }
   });
 
   ws.on("message", (msg) => {
-    socket.write(msg);
+    try {
+      socket.write(msg);
+    } catch (e) {
+      console.log("[WRITE ERROR]", e.message);
+    }
   });
 
   socket.on("close", () => {
+    console.log("[SSH] Conexão fechada");
     ws.close();
   });
 
   ws.on("close", () => {
+    console.log("[WS] Cliente desconectado");
     socket.destroy();
   });
 
   socket.on("error", (err) => {
-    console.log("Socket Error:", err.message);
+    console.log("[SSH ERROR]", err.message);
     ws.close();
   });
 });
